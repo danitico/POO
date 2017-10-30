281f2c17e83041e4ea2718178e3242f56bb36f27
danitico <i62rapad@uco.es>
2017-10-30 15:31:43 +0100

    v2.0

--- P3/ClaseRuleta/jugador.h
+++ P3/ClaseRuleta/jugador.h
@@ -6,6 +6,12 @@ struct Apuesta{
 	std::string valor;
 	int cantidad;
 };
+/*
+ * TAD Jugador
+ * DESCRIPCIÓN El TAD Jugador hereda de la clase Persona. Representa la información
+ * y datos de un jugador de una ruleta
+ * OPERACIONES 
+ */
 class Jugador : public Persona{
 	private:
 		int dinero_;
@@ -16,21 +22,57 @@ class Jugador : public Persona{
 			code_=code;
 			dinero_=1000;
 		}
+		/*
+		 *	PROC getCodigo() DEV (string code_)
+		 *	REQUIERE True
+		 *	MODIFICA 0
+		 *	EFECTOS Devuelve la cadena code_
+		 */
 		std::string getCodigo() const{
 			return code_;
 		}
+		/*
+		 *	PROC setCodigo(string code) DEV ()
+		 *	REQUIERE True
+		 *	MODIFICA code_
+		 *	EFECTOS El dato privado code_ recibe el valor del parámetro
+		 */
 		void setCodigo(std::string code){
 			code_=code;
 		}
+		/*
+		 *	PROC getDinero() DEV (int dinero_)
+		 *	REQUIERE True
+		 *	MODIFICA 0
+		 *	EFECTOS Devuelve el entero dinero_
+		 */
 		int getDinero() const{
 			return dinero_;
 		}
+		/*
+		 *	PROC setDinero(int dinero) DEV ()
+		 *	REQUIERE True
+		 *	MODIFICA dinero_
+		 *	EFECTOS El dato privado code_ recibe el valor del parámetro
+		 */
 		void setDinero(int dinero){
 			dinero_=dinero;
 		}
+		/*
+		 *	PROC getApuestas() DEV (list<Apuestas> apuestas_)
+		 *	REQUIERE True
+		 *	MODIFICA 0
+		 *	EFECTOS El dato privado code_ recibe el valor del parámetro
+		 */
 		std::list<Apuesta> getApuestas() const{
 			return apuestas_;
 		}
+		/*
+		 *	PROC setApuestas() DEV ()
+		 *	REQUIERE True
+		 *	MODIFICA apuestas_
+		 *	EFECTOS Guarda los datos de los ficheros de apuestas en una lista de apuestas
+		 */
 		void setApuestas();
 };
 #endif
