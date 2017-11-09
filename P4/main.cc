c737f77d628656cdf9844acbea1148a3608921b5
danitico <danitico98@gmail.com>
2017-11-09 16:52:52 +0100

    ok

--- P4/main.cc
+++ P4/main.cc
@@ -1,13 +1,16 @@
-#include <ruleta.h>
-#include <crupier.h>
-#include <jugador.h>
-#include <persona.h>
+#include <list>
+#include "ruleta.h"
+#include "crupier.h"
+#include "jugador.h"
+#include "persona.h"
 #include <iostream>
-void menu(int opcion);
+#include <cstdlib>
 void printstatus(int &nj, int &suma, int &n, int &beneficio);
 int main(){
 	Crupier crupier("1234", "0001");
 	Ruleta ruleta(crupier);
+	Jugador jugador("","");
+	std::string DNI;
 
 	int opcion, nj, suma, n, beneficio;
 	opcion=nj=suma=n=beneficio=0;
@@ -23,47 +26,47 @@ int main(){
 	
 		std::cout<<"Opción---> ";
 		std::cin>>opcion;
+		
+		switch(opcion){
 
-		menu(opcion);
-	}
-}
-void menu(int opcion){
-	switch(opcion){
+			case 1:
+				ruleta.leeJugadores();
+				break;
 
-		case 1:
-			ruleta.leeJugadores();
-			break;
+			case 2:
+				ruleta.escribeJugadores();
+				break;
 
-		case 2:
-			ruleta.escribeJugadores();
-			break;
+			case 3:
+				ruleta.getEstadoRuleta(nj, suma, n, beneficio);
+				printstatus(nj, suma, n, beneficio);
+				break;
 
-		case 3:
-			ruleta.getEstadoRuleta(nj, suma, n, beneficio);
-			ruleta.printstatus(nj, suma, n, beneficio);
-			break;
-
-		case 4:
+			case 4:
 			
 
 
 
-		case 5:
-			//datos del jugador a borrar
-			ruleta.deleteJugador(std::string DNI);
-			break;
-		case 6:
-			//Datos jugador
-			if(ruleta.addJugador(jugador)) std::cout<<"éxito\n";
-			else std::cout<<"failed to add it\n";
-			break;
+			case 5:
+				//datos DNI
+				ruleta.deleteJugador(DNI);
+				break;
+			case 6:
+				//Datos jugador sobrecarga extractor
+				if(ruleta.addJugador(jugador)) std::cout<<"éxito\n";
+				else std::cout<<"failed to add it\n";
+				break;
 
-
+			default:
+				std::cout<<"Opción Incorrecta"<<std::endl;
+				break;
+		}
+	}
+	std::cout<<"Cerrado con éxito\n";		
 }
 void printstatus(int &nj, int &suma, int &n, int &beneficio){
-	printf("\t\tNumero jugadores ---->%i\n", nj);
-	printf("\t\tDinero en la mesa --->%i\n", suma);
-	printf("\t\tNumero lanzamientos ->%i\n", n);
-	printf("\t\tSaldo Banca --------->%i\n", beneficio);
-	//por favor DANIEL, hazlo en c++ hijo mio
+	std::cout<<"\t\tNumero jugadores ---->"<<nj<<std::endl;
+	std::cout<<"\t\tDinero en la mesa --->"<<suma<<std::endl;
+	std::cout<<"\t\tNumero lanzamientos ->"<<n<<std::endl;
+	std::cout<<"\t\tSaldo Banca --------->"<<beneficio<<std::endl;
 }
